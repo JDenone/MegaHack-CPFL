6561430a270ef81f2d9c9a3502a11e18f2a1047e
romulo.lara <romulo.lara@gmail.com>
2020-11-14 17:30:24 -0300

    API: rota de historico do cliente + knex

---
+++ backend/db/tabelas.js
@@ -0,0 +1,4 @@
+module.exports = {
+  clientes: "clientes",
+  historico: "historico"
+};
--- backend/src/rotas/cliente.js
+++ backend/src/rotas/cliente.js
@@ -2,17 +2,29 @@ const knex = require("../knex");
 const express = require("express");
 const rota = express.Router();
 
-const TABELA = "clientes";
+const TABELAS = require("../../db/tabelas");
 
 rota.route("/").get(async (req, res) => {
-  const clientes = await knex.select().table(TABELA);
+  const clientes = await knex.select().table(TABELAS.clientes);
   res.json([...clientes]);
 });
 
 rota.route("/:cpf").get(async (req, res) => {
   const { cpf } = req.params;
-  const cliente = await knex.select().table(TABELA).where({ cpf: cpf });
+  const cliente = await knex
+    .select()
+    .table(TABELAS.clientes)
+    .where({ cpf: cpf });
   res.json({ ...cliente });
+});
+
+rota.route("/:cpf/historico").get(async (req, res) => {
+  const { cpf } = req.params;
+  const historico = await knex
+    .select()
+    .table(TABELAS.historico)
+    .where({ cpf_cliente: cpf });
+  res.json({ ...historico });
 });
 
 module.exports = rota;
